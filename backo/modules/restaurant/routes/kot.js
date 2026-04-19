295d49dd5db593c84e74e567d85e5563fee57872
Arshath <elyasarshath6963gmail.com>
2026-04-19 21:54:13 +0530

    check render

--- backo/modules/restaurant/routes/kot.js
+++ backo/modules/restaurant/routes/kot.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const KOT = require("../models/Kot");
-
+const KOT = require("../models/kot");
 
 router.post("/create", async (req, res) => {
     try {
