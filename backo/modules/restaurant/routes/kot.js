834768b197df91d963d0182873ccd242f250f259
Arshath <elyasarshath6963gmail.com>
2026-04-19 21:38:04 +0530

    change kot

--- backo/modules/restaurant/routes/kot.js
+++ backo/modules/restaurant/routes/kot.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const KOT = require("../models/KOT");
+const KOT = require("../models/Kot");
 
 
 router.post("/create", async (req, res) => {
